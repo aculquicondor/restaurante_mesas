eb19e1da61cbbda3ef909496fe7f2bbf405f81e9
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-04-15 23:14:18 -0500

    Client, class and mapping

---
+++ src/Restaurant/TablesBundle/Document/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+// src/Restaurant/TablesBundle/Document/Cliente.php
+namespace Restaurant\TablesBundle\Document;
+
+class Client
+{
+    protected $id;
+    protected $dni;
+    protected $ruc;
+    protected $name;
+    protected $address;
+}

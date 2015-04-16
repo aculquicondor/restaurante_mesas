28d80e7680e53550afbcc94f649a322f376aa7b2
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-04-15 23:16:18 -0500

    Table, class and mapping

---
+++ src/Restaurant/TablesBundle/Document/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+// src/Restaurant/TablesBundle/Document/Cliente.php
+namespace Restaurant\TablesBundle\Document;
+
+class Table
+{
+    protected $id;
+    protected $available;
+    protected $occupation_time;
+    protected $capacity;
+}

504ad59d6dc98d019693b8d3920d12b47f9bcf4b
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-04-17 23:59:21 -0500

    #7-ItemOrder

---
+++ src/Restaurant/TablesBundle/Document/ItemOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+// src/Restaurant/TablesBundle/Document/ItemOrder.php
+namespace Restaurant\TablesBundle\Document;
+
+class ItemOrder
+{
+    protected $menu_item;
+    protected $observations;
+
+}

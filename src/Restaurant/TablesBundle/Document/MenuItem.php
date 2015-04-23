107aad98e020de649b0c0ac1de318ddb288da299
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-04-23 18:34:42 -0500

    Fixing OrderItem yml & generating getters&setters

--- src/Restaurant/TablesBundle/Document/MenuItem.php
+++ src/Restaurant/TablesBundle/Document/MenuItem.php
@@ -9,5 +9,80 @@ class MenuItem
     protected $available;
     protected $price;
 
-}
 
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set available
+     *
+     * @param boolean $available
+     * @return self
+     */
+    public function setAvailable($available)
+    {
+        $this->available = $available;
+        return $this;
+    }
+
+    /**
+     * Get available
+     *
+     * @return boolean $available
+     */
+    public function getAvailable()
+    {
+        return $this->available;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return self
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float $price
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
--- src/Restaurant/TablesBundle/Document/OrderItem.php
+++ src/Restaurant/TablesBundle/Document/OrderItem.php
@@ -9,4 +9,58 @@ class OrderItem
     protected $menu_item;
     protected $observations;
 
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set observations
+     *
+     * @param string $observations
+     * @return self
+     */
+    public function setObservations($observations)
+    {
+        $this->observations = $observations;
+        return $this;
+    }
+
+    /**
+     * Get observations
+     *
+     * @return string $observations
+     */
+    public function getObservations()
+    {
+        return $this->observations;
+    }
+
+    /**
+     * Set menuItem
+     *
+     * @param TablesBundle\Document\MenuItem $menuItem
+     * @return self
+     */
+    public function setMenuItem(\TablesBundle\Document\MenuItem $menuItem)
+    {
+        $this->menu_item = $menuItem;
+        return $this;
+    }
+
+    /**
+     * Get menuItem
+     *
+     * @return TablesBundle\Document\MenuItem $menuItem
+     */
+    public function getMenuItem()
+    {
+        return $this->menu_item;
+    }
 }

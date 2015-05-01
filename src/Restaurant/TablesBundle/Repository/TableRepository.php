1764eece2c5c699119b1b2d9d5d945451570ef3e
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-05-01 18:50:03 -0500

    #13-separating Table repository and Reservation Repository.

--- src/Restaurant/TablesBundle/Repository/TableRepository.php
+++ src/Restaurant/TablesBundle/Repository/TableRepository.php
@@ -6,9 +6,6 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
 
 class TableRepository extends DocumentRepository
 {
-    public function getTableById($id){
-        return $this->findOneBy(array('id' => $id));
-    }
     public function getAvailableTables(){
         return $this->findBy(array('available' => 1));
     }

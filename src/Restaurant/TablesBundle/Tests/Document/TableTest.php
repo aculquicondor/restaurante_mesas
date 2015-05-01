1342096cb82370716615a4d52192fe8568f00567
Rudy Godoy <rudygodoyg@gmail.com>
2015-04-30 21:13:44 -0500

    Pruebas para clase Table. Refs #17

---
+++ src/Restaurant/TablesBundle/Tests/Document/TableTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Restaurant\TablesBundle\Tests\Document;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use \Restaurant\TablesBundle\Document\Table;
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+
+class TableTest extends KernelTestCase {
+
+    /**
+     * @var DocumentManager
+     */
+    private static $dm;
+
+    /**
+     * @var Table
+     */
+    private $table;
+
+    /**
+     * @var inheritDoc
+     */
+    public static function setUpBeforeClass()
+    {
+        self::bootKernel();
+        self::$dm = static::$kernel->getContainer()
+            ->get('platform.user.manager');
+    }
+
+    /**
+     * @var inheritDoc
+     */
+    public function setUp()
+    {
+        $this->table = new Table();
+        $this->table->setOccupationTime(30);
+        $this->table->setCapacity(4);
+        $this->table->setAvailable(true);
+    }
+
+    public function testPersistence()
+    {
+        self::$dm->persist($this->table);
+        self::$dm->flush();
+    }
+
+    public function testRemove()
+    {
+        self::$dm->remove($this->table->getId());
+        self::$dm->flush();
+    }
+
+    /**
+     * @var inheritDoc
+     */
+    public static function tearDownAfterClass()
+    {
+        parent::tearDownAfterClass();
+        self::$dm->close();
+    }
+}

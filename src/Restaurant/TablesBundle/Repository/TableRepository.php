38391838f8e2377406344e906d556976697c9477
Paulo Ramirez <paulo.ramirez@ucsp.edu.pe>
2015-05-02 11:22:27 -0500

    Revert "Fixing"
    This reverts commit 2f98b7c0ff00ad5d9d0a177ff28aad40c430b5f0.

--- src/Restaurant/TablesBundle/Repository/TableRepository.php
+++ src/Restaurant/TablesBundle/Repository/TableRepository.php
@@ -4,16 +4,18 @@ namespace Restaurant\TablesBundle\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
 
-class ReservationRepository extends DocumentRepository
+class TableRepository extends DocumentRepository
 {
-    public function getReservationForTableNow(\Restaurant\TablesBundle\Document\Table $table, date $now)
-    {
-        $reservations = $this->createQueryBuilder()
-            ->field('date')->gte($now->sub(new \DateInterval('P1H')))
-            ->lte($now->add(new \DateInterval('P1H')))
-            ->field('tables')->includesReferenceTo($table)
-            ->getQuery()
-            ->execute();
-        return $reservations;
+    public function getAvailableTables(){
+        $tables = $this->findBy(array('available' => 1));
+        $usable_reservations = $this->get('doctrine_mongodb')
+            ->getRepository('RestaurantTablesBundle:Reservation')
+            ->getAvailableReservationsForUse();
+        $available_tables = array();
+        foreach($usable_reservations as $reservation){
+            $available_tables[] = $reservation->getTables();
+        }
+        $available_tables = $available_tables + $tables;
+        return $available_tables;
     }
 }

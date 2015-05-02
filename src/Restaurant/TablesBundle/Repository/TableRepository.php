fdb0a182a2fce04614ebb5b93e0f3d477279adfa
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-05-01 23:36:18 -0500

    #13-Available tables

--- src/Restaurant/TablesBundle/Repository/TableRepository.php
+++ src/Restaurant/TablesBundle/Repository/TableRepository.php
@@ -7,6 +7,15 @@ use Doctrine\ODM\MongoDB\DocumentRepository;
 class TableRepository extends DocumentRepository
 {
     public function getAvailableTables(){
-        return $this->findBy(array('available' => 1));
+        $tables = $this->findBy(array('available' => 1));
+        $usable_reservations = $this->get('doctrine_mongodb')
+            ->getRepository('RestaurantTablesBundle:Reservation')
+            ->getAvailableReservationsForUse();
+        $available_tables = array();
+        foreach($usable_reservations as $reservation){
+            $available_tables[] = $reservation->getTables();
+        }
+        $available_tables = $available_tables + $tables;
+        return $available_tables;
     }
 }

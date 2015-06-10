8ad5bb2233b53dca9dd41479a6ee07aac3b2cd17
Rudy Godoy <rudygodoyg@gmail.com>
2015-06-09 21:28:37 -0500

    Eliminado configuración usando anotaciones Refs. #38

--- src/Restaurant/TablesBundle/Document/Order.php
+++ src/Restaurant/TablesBundle/Document/Order.php
@@ -4,8 +4,6 @@ namespace Restaurant\TablesBundle\Document;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ODM\MongoDB\Mapping\Annotations\EmbedMany;
-
 
 class Order {
 

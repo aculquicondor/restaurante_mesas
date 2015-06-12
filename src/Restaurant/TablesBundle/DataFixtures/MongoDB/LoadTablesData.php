bc5de89886a96da2e18a7bc048fbd635440b9cac
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-06-12 10:32:00 -0500

    campo `number` en fixture Table

--- src/Restaurant/TablesBundle/DataFixtures/MongoDB/LoadTablesData.php
+++ src/Restaurant/TablesBundle/DataFixtures/MongoDB/LoadTablesData.php
@@ -16,28 +16,33 @@ class LoadTablesData extends AbstractFixture
     public function load(ObjectManager $manager)
     {
         $table1 = new Table();
+        $table1->setNumber(1);
         $table1->setAvailable(true);
         $table1->setCapacity(4);
         $manager->persist($table1);
 
         $table2 = new Table();
+        $table2->setNumber(2);
         $table2->setAvailable(false);
         $table2->setOccupationTime(new \DateTime('2015-05-06 09:00'));
         $table2->setCapacity(4);
         $manager->persist($table2);
 
         $table3 = new Table();
+        $table3->setNumber(3);
         $table3->setAvailable(false);
         $table3->setOccupationTime(new \DateTime('2015-05-08 10:00'));
         $table3->setCapacity(4);
         $manager->persist($table3);
 
         $table4 = new Table();
+        $table4->setNumber(4);
         $table4->setAvailable(true);
         $table4->setCapacity(4);
         $manager->persist($table4);
 
         $table5 = new Table();
+        $table5->setNumber(5);
         $table5->setAvailable(true);
         $table5->setCapacity(4);
         $manager->persist($table5);

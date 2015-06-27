074f85abf579f5733105e16b1452526152dd7c2b
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-06-27 14:13:30 -0500

    usando preprocess y uglify
    Además se eliminó carpetas innecesarias para controladores y servicios

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,48 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    concat: {
-      options: {
-        separator: '\n'
-      },
-      dist: {
-        src: [
-            'web/app/js/controllers.js',
-            'web/app/js/controllers/menu_items/*.js',
-            'web/app/js/controllers/orders/*.js',
-            'web/app/js/controllers/stores/*.js',
-            'web/app/js/controllers/tables/*.js',
-            'web/app/js/services.js',
-            'web/app/js/services/menu_items/*.js',
-            'web/app/js/services/orders/*.js',
-            'web/app/js/services/reservations/*.js',
-            'web/app/js/services/stores/*.js',
-            'web/app/js/services/tables/*.js',
-            'web/app/js/main.js'
-        ],
-        dest: 'web/app/js/all.js'
-      }
-    },
-      jshint: {
-      files: ['Gruntfile.js', 'web/app/js/*.js'],
-      options: {
-        globals: {
-          jQuery: true
+    grunt.initConfig({
+        env: {
+            options: {},
+            debug: {
+                LOCAL: 'YES',
+                UGLIFY: 'NO'
+            },
+            dev: {
+                LOCAL: 'YES',
+                UGLIFY: 'YES'
+            },
+            prod: {
+                LOCAL: 'NO',
+                UGLIFY: 'YES'
+            }
+        },
+        uglify: {
+            all_js: {
+                files: {
+                    'web/app/js/all.js': [
+                        'web/app/js/main.js',
+                        'web/app/js/controllers.js',
+                        'web/app/js/controllers/*.js',
+                        'web/app/js/services.js',
+                        'web/app/js/services/*.js'
+                    ]
+                }
+            }
+        },
+        preprocess: {
+            html: {
+                src: 'web/app/app.html',
+                dest: 'web/app/index.html'
+            }
         }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-preprocess');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['concat']);
+    grunt.registerTask('debug', ['env:debug', 'preprocess']);
+    grunt.registerTask('dev', ['env:dev', 'uglify', 'preprocess']);
+    grunt.registerTask('prod', ['env:prod', 'uglify', 'preprocess']);
+    grunt.registerTask('default', ['dev']);
 
 };

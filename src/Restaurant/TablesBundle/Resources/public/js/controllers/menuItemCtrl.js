2c0cd70853c99010b0b84df81eafa98fd3987896
Rudy Godoy <rudygodoyg@gmail.com>
2015-06-29 14:58:37 -0500

    Como nuestro JSON retorna un array con nombre y dentro los elementos, hay que usarlo en la iteración.

--- src/Restaurant/TablesBundle/Resources/public/js/controllers/menuItemCtrl.js
+++ src/Restaurant/TablesBundle/Resources/public/js/controllers/menuItemCtrl.js
@@ -1,7 +1,4 @@
 restaurantControllers.controller('MenuItemCtrl', ['$scope', 'MenuItem', function($scope, MenuItem) {
-    MenuItem.getAll().$promise.then(function(items) {
-        $scope.items = items.items
-    }, function (errResponse) {
-        console.log(errResponse);
-    });
+    $scope.items = MenuItem.query();
+    $scope.orderProp = 'available';
 }]);
--- src/Restaurant/TablesBundle/Resources/public/js/services/menuItemSvc.js
+++ src/Restaurant/TablesBundle/Resources/public/js/services/menuItemSvc.js
@@ -1,6 +1,6 @@
 restaurantServices.factory('MenuItem', ['$resource',
     function($resource){
         return $resource('http://localhost:8000/api/menu/items.json', {}, {
-            getAll: {method: 'GET'}
+            query: {method: 'GET', isArray: false}
         });
     }]);

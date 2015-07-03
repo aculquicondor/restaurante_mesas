5b0c2520a4e4a367f7093292066d5d5560331589
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-07-03 12:00:53 -0500

    redirecting if not authenticated

--- src/Restaurant/TablesBundle/Resources/public/js/controllers/tableCtrl.js
+++ src/Restaurant/TablesBundle/Resources/public/js/controllers/tableCtrl.js
@@ -1,3 +1,7 @@
-restaurantControllers.controller('TablesCtrl', ['$scope', 'Tables', function ($scope, Tables) {
-    $scope.tables = Tables.query();
-}]);
+restaurantControllers.controller('TablesCtrl', ['$scope', 'Tables', 'AuthSvc', '$location',
+    function ($scope, Tables, AuthSvc, $location) {
+        if (!AuthSvc.isAuthenticated()) {
+            $location.path('/login');
+        }
+        $scope.tables = Tables.query();
+    }]);

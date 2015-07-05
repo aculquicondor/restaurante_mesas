26ba59a0673a35644850cefa37312f09c8ecdd5e
Paulo <paulo.ramirez@ucsp.edu.pe>
2015-07-05 09:44:33 -0500

    Corrigiendo controlador y servicio

--- src/Restaurant/TablesBundle/Resources/public/js/controllers/tableCtrl.js
+++ src/Restaurant/TablesBundle/Resources/public/js/controllers/tableCtrl.js
@@ -1,12 +1,12 @@
-restaurantControllers.controller('TablesCtrl', ['$scope', 'Tables', 'AuthSvc', '$location', '$sce',
-    function ($scope, Tables, AuthSvc, $location) {
+restaurantControllers.controller('TablesCtrl', ['$scope', 'Tables', 'Table', 'AuthSvc', '$location', '$sce',
+    function ($scope, Tables, Table, AuthSvc, $location) {
         if (!AuthSvc.isAuthenticated()) {
             $location.path('/login');
         }
         $scope.tables = Tables.query();
         $scope.availableSwitch = false;
         $scope.update = function (tableId, availableSwitch) {
-            Tables.update({}, {id: tableId, available: availableSwitch}, function (table) {
+            Table.update({tableId: tableId}, {available: availableSwitch}, function (table) {
                 console.log(table);
             });
         }

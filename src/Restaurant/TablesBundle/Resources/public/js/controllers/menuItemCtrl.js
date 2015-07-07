965e9cf68b121e51a9518b872d723f752c009fcd
Rudy Godoy <rudygodoyg@gmail.com>
2015-07-06 21:35:29 -0500

    Lista de menu items reconstruida. #108

--- src/Restaurant/TablesBundle/Resources/public/js/controllers/menuItemCtrl.js
+++ src/Restaurant/TablesBundle/Resources/public/js/controllers/menuItemCtrl.js
@@ -1,14 +1,42 @@
-restaurantControllers.controller('MenuItemListCtrl', ['$scope', '$rootScope', 'MenuItems',
-    function($scope, $rootScope, MenuItems) {
-        $rootScope.section = 'Menu';
+restaurantControllers.controller('MenuItemListCtrl', ['$scope', '$rootScope', 'MenuItems', '$location', 'AuthSvc',
+    function($scope, $rootScope, MenuItems, $location, AuthSvc) {
+        if (!AuthSvc.isAuthenticated()) {
+            $location.path('/login');
+        }
 
+        $rootScope.section = 'Menu';
         $scope.items = MenuItems.query();
-        $scope.orderProp = 'available';
+        $scope.orderProperty = 'available';
+        $scope.orderReverse = true;
+        $scope.getParams = {
+            available: false
+        };
+
+        $scope.doQuery = function () {
+            var params = {
+                available: $scope.getParams.available ? 1 : 0
+            };
+            $scope.items = MenuItems.query(params);
+        }
+
     }]);
 
-restaurantControllers.controller('MenuItemDetailCtrl', ['$scope', '$rootScope', '$routeParams', 'MenuItem',
-    function($scope, $rootScope, $routeParams, MenuItem) {
+restaurantControllers.controller('MenuItemDetailCtrl', ['$scope', '$rootScope', '$routeParams', 'MenuItem', '$location', 'AuthSvc',
+    function($scope, $rootScope, $routeParams, MenuItem, $location, AuthSvc) {
+        if (!AuthSv.isAuthenticated()){
+            $location.path('/login');
+        }
         $rootScope.section = 'Menu';
 
-        $scope.item = MenuItem.query({itemId: $routeParams.itemId});
+        $scope.item = MenuItem.get({itemId: $routeParams.itemId});
+
+        $scope.deleteMenuItem = function () {
+            MenuItem.delete({itemId: $scope.item.id}, {}, function () {
+                $location.path('/menu');
+            });
+        };
+
+        $scope.itemChange = function (item) {
+            MenuItem.update({itemId: $scope.item.id})
+        };
     }]);
